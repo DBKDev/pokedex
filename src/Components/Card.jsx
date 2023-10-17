e51a84756b40a1827e64c4f8e47b2efc7c40d91e
DBK <DBKDEV@outlook.fr>
2023-10-17 21:06:58 +0200

    17/10/2023 21h06

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,12 +3,13 @@ import "../Styles/Card.css"
 
 const Card = () => {
     return ( 
-        <div className="Card-content">        
+        <div id="Card-content">        
             <div className="Card-Pokemon">
-
             </div>
             <div className="Card-infos">
-
+                <h1>Bulbizarre</h1>
+                <p className="type-pokemon">type de pokemon</p>
+                <p className="localisation-pokemon">Localisation</p>
             </div>
         </div>
      );
